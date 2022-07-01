a8408c435886ede35fde7a7d2ac4f8adfb56afe5
rokas <rokasra@gmail.com>
2022-07-01 09:36:03 +0300

    User class based

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,14 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  render() {
+    return <l1 className={classes.user}>{this.props.name}</l1>;
+  }
+}
+
+// const User1 = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
