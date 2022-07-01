003f3c3959c0c96d734619140d3b75924fda5d3d
rokas <rokasra@gmail.com>
2022-07-01 10:21:50 +0300

    UserFinder

---
+++ src/components/UserFinder.js
@@ -0,0 +1,36 @@
+import css from './UserFinder.module.css';
+import { useState, useEffect } from 'react';
+import Users from './Users';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'James' },
+  { id: 'u2', name: 'Mike' },
+  { id: 'u3', name: 'Jane' },
+];
+
+function UserFinder() {
+  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchChangeHandler = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // componentWillUpdate - class based
+  useEffect(() => {
+    console.log('searchTerm updated', searchTerm);
+    const filteredArr = DUMMY_USERS.filter((userObj) => userObj.name.includes(searchTerm));
+    setFilteredUsers(filteredArr);
+  }, [searchTerm]);
+
+  return (
+    <>
+      <div className={css.finder}>
+        <input type='search' value={searchTerm} onChange={searchChangeHandler} />
+      </div>
+      <Users users={filteredUsers} />
+    </>
+  );
+}
+
+export default UserFinder;
